a1416ed9370b9753651d0e0a916e7ae8ca0fb520
Jeremy Lazo <jeremylazo@berkeley.edu>
2025-02-23 15:31:23 -0800

    added django-select2 back lol

--- USQuery/USQuery/settings.py
+++ USQuery/USQuery/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
+    'django_select2',
     'bootstrap5',
     'aiohttp',
     'dj_database_url',
