73c95834caf16a90296d9efeab845e0044b227dd
Jeremy Lazo <jeremylazo@berkeley.edu>
2025-12-22 02:07:11 -0800

    Graphql limiting. Made graphql api batch fetch utils have a in parallel task limit. Updated graphql bills query to only load summaries if argument is present in query, also updated code to no longer try to fetch ai summaries for batch requets.

--- USQuery/strawberryAPI/graphql/schema.py
+++ USQuery/strawberryAPI/graphql/schema.py
@@ -1,4 +1,8 @@
 import strawberry
+from strawberry.extensions import QueryDepthLimiter
+from strawberry.schema.config import StrawberryConfig
 from .queries import Query
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(query=Query,
+        extensions=[QueryDepthLimiter(max_depth=5)],
+        config=StrawberryConfig(batching_config={"max_operations": 3}))
