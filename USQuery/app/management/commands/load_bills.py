178ab204b70ccbbc10076a447b8e1021fa81a33a
Jeremy Lazo <jeremylazo@berkeley.edu>
2026-03-07 01:46:00 -0800

    added user choice to recieve subject and/or bill notif updates.

--- USQuery/app/management/commands/load_bills.py
+++ USQuery/app/management/commands/load_bills.py
@@ -1,6 +1,8 @@
 import asyncio
+from django.core.cache import cache
 from django.core.management.base import BaseCommand
 from app.utils import updateRecentBills, types
+from notifications.push import send_subject_notification
 
 class Command(BaseCommand):
     help = "Load data from congress API into database"
@@ -9,4 +11,11 @@ class Command(BaseCommand):
         self.stdout.write("start load")
         for t in types :
             asyncio.run(updateRecentBills(119, "!", t))
+        subjects = cache.get("bill_subjects", set())
+        asyncio.run(send_subject_notification(
+            subjects=subjects,
+            title="New actions on bills you might be interested in.",
+            body="New actions were made on bills related to your favorite subjects."
+        ))
+        cache.delete("bill_subjects")
         self.stdout.write("Data load completed")
