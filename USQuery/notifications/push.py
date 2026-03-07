608edc9500335961391089c9e59e8e4665c917c8
Jeremy Lazo <jeremylazo@berkeley.edu>
2026-03-07 01:46:00 -0800

    added user choice to recieve subject and/or bill notif updates.

--- USQuery/notifications/push.py
+++ USQuery/notifications/push.py
@@ -1,5 +1,5 @@
 import requests
-from .models import Device, StarredBill
+from .models import Device, FavoriteSubject, StarredBill
 from asgiref.sync import sync_to_async
 
 EXPO_PUSH_URL = "https://exp.host/--/api/v2/push/send"
@@ -13,7 +13,7 @@ async def send_bill_notification(bill_id, title, body):
     if not users:
         return
     tokens = await sync_to_async(list)(
-        Device.objects.filter(user_profile__id__in=users, is_active=True).values_list("device_token", flat=True)
+        Device.objects.filter(user_profile__id__in=users, user_profile__enabled_bill_notif=True, is_active=True).values_list("device_token", flat=True)
     )
     if not tokens:
         return
@@ -28,6 +28,34 @@ async def send_bill_notification(bill_id, title, body):
                 "title": title,
                 "body": body,
                 "data": {"bill_id": bill_id},
+            }
+            for token in batch_tokens
+        ]
+
+        # Run the blocking HTTP call in a thread (sync_to_async) so we don't block the event loop
+        await sync_to_async(requests.post)(EXPO_PUSH_URL, json=messages)
+
+async def send_subject_notification(subjects, title, body):
+    users = await sync_to_async(set)(
+        FavoriteSubject.objects.filter(subject_id__in=subjects).values_list("user_profile__id", flat=True)
+    )
+    if not users:
+        return
+    tokens = await sync_to_async(list)(
+        Device.objects.filter(user_profile__id__in=users, user_profile__enabled_subject_notif=True, is_active=True).values_list("device_token", flat=True)
+    )
+    if not tokens:
+        return
+
+    # Build and send messages in Expo-compatible batches
+    for i in range(0, len(tokens), EXPO_BATCH_SIZE):
+        batch_tokens = tokens[i : i + EXPO_BATCH_SIZE]
+        messages = [
+            {
+                "to": token,
+                "sound": "default",
+                "title": title,
+                "body": body,
             }
             for token in batch_tokens
         ]
