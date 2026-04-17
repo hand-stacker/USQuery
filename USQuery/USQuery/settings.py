4bc1e62c09b2f08d501f86f86a639a8b97ccafb0
Jeremy Lazo <jeremylazo@berkeley.edu>
2026-04-17 10:20:18 -0700

    static versioning

--- USQuery/USQuery/settings.py
+++ USQuery/USQuery/settings.py
@@ -172,7 +172,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
